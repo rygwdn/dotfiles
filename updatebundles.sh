01b6edc4eaae3467c9295fdc1c75ffb977f3bcd2
Ryan W <rygwdn@gmail.com>
2011-01-21 08:24:25 -0400

    quick-fix for updatebundles

--- updatebundles.sh
+++ updatebundles.sh
@@ -2,4 +2,16 @@
 
 git submodule foreach git checkout master
 git submodule foreach git pull
+echo
+
 vim -e -c 'BundleInstall' -c 'q'
+echo
+
+for dir in ~/.vim/bundle/*
+do
+    (
+        cd $dir
+        echo $dir
+        git pull
+    )
+done
