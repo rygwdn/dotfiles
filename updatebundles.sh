f5d0b9580f2256d39f5c3eff32190592852a85d1
Ryan W <rygwdn@gmail.com>
2011-04-29 11:56:09 -0300

    working on using new vundle stuff

--- updatebundles.sh
+++ updatebundles.sh
@@ -1,29 +1,14 @@
 #!/bin/sh
 
-"Echo Init"
+echo "Init"
 base="$1"
 [ -z "$base" ] && [ -d vimrc ] && base=`pwd`
 [ -z "$base" ] && base=~/.vim
+[ -d $base/autoload ] || mkdir $base/autoload
+[ -d $base/bundle ]   || mkdir $base/bundle
 
 git submodule foreach git checkout master
 git submodule foreach git pull
-mkdir $base/autoload
-mkdir $base/bundle
-#cp $base/lib/pathogen/autoload/* $base/autoload
-echo
-
-vim -e -c 'BundleInstall' -c 'q'
-echo
-
-echo "Update bundles"
-for dir in $base/bundle/*
-do
-    (
-        cd $dir
-        echo $dir
-        git pull
-    )
-done
 echo
 
 echo Update autoloads
@@ -31,3 +16,15 @@ for dir in $base/lib/*
 do
     cp -r $dir/autoload/* $base/autoload/
 done
+echo
+
+echo "Update bundles"
+vim -e -c 'BundleInstall!' -c 'q'
+#for dir in $base/bundle/*
+#do
+#    (
+#        cd $dir
+#        echo $dir
+#        git pull
+#    )
+#done
