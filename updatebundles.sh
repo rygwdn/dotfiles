b90927e12d9013aaccaf7103bab10810eb29b7a5
Ryan W <rygwdn@gmail.com>
2011-05-05 14:13:23 -0300

    updated updater :)

--- updatebundles.sh
+++ updatebundles.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
+install=false
+[ "$1" = "-i" ] && shift && install=true
+
 echo "Init"
 base="$1"
 [ -z "$base" ] && [ -d vimrc ] && base=`pwd`
 [ -z "$base" ] && base=~/.vim
+
 [ -d $base/autoload ] || mkdir $base/autoload
 [ -d $base/bundle ]   || mkdir $base/bundle
 
+# pathogen and vundle
 git submodule foreach git checkout master
 git submodule foreach git pull
 echo
@@ -16,12 +21,7 @@ cp -r $base/lib/*/autoload/* $base/autoload/
 echo
 
 echo "Update bundles"
-vim -e -c 'BundleInstall!' -c 'q'
-#for dir in $base/bundle/*
-#do
-#    (
-#        cd $dir
-#        echo $dir
-#        git pull
-#    )
-#done
+$install && bi='BundleInstall' || bi='BundleInstall!'
+vim -u bundles.vim -e -c $bi -c 'q'
+
+exit 0
