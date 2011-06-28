bd00ce2f3cd2c8f31549c054b6c6bc7d043981ff
Ryan W <rygwdn@gmail.com>
2011-06-28 11:23:30 +0100

    fixed whitespace in paths and allow no-bundle init

--- updatebundles.sh
+++ updatebundles.sh
@@ -3,13 +3,19 @@
 install=false
 [ "$1" = "-i" ] && shift && install=true
 
+nobundle=false
+[ "$1" = "-n" ] && shift && nobundle=true
+
 echo "Init"
 base="$1"
-[ -z "$base" ] && [ -d vimrc ] && base=`pwd`
+[ -z "$base" ] && [ -e vimrc ] && base=`pwd`
 [ -z "$base" ] && base=~/.vim
 
-[ -d $base/autoload ] || mkdir $base/autoload
-[ -d $base/bundle ]   || mkdir $base/bundle
+[ -d "$base/autoload" ] || mkdir "$base/autoload"
+[ -d "$base/bundle" ]   || mkdir "$base/bundle"
+
+$install && git submodule init
+$install && git submodule update
 
 # pathogen and vundle
 git submodule foreach git checkout master
@@ -17,8 +23,10 @@ git submodule foreach git pull
 echo
 
 echo Update autoloads
-cp -r $base/lib/*/autoload/* $base/autoload/
+cp -r "$base"/lib/*/autoload/* "$base/autoload/"
 echo
+
+$nobundle && echo "Now to :BundleInstall from vim" && exit 0
 
 echo "Update bundles"
 $install && bi='BundleInstall' || bi='BundleInstall!'
