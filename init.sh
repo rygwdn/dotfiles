679de49e6ea6421065fdca8ddedf653ae423f34c
Ryan W <rygwdn@gmail.com>
2011-08-26 19:47:40 +0100

    improvements to init.sh.
    - test for windows (tested on win7)
    - make mklink work (when run in administrative prompt)
    - fixed case where link exists
    - explicit check for $dir/"$dir"rc instead of */*rc
    - ignore init.sh
    - special case vim and vimrc on windows

--- init.sh
+++ init.sh
@@ -1,26 +1,36 @@
 #!/bin/bash
 
-if test "$1" = "cp"
+if test "$1" = "win" || test "$OS" = "Windows_NT"
 then
     win="true"
 else
     win="false"
 fi
 
+mklink() { cmd /c mklink "$@"; }
+
 function dolink()
 {
     file=$1
     hf=$HOME/.`basename $file`
+    if $win
+    then
+        [ $file = "vim" ] && hf=$HOME/vimfiles
+        [ $file = "vim/vimrc" ] && file="vim/_vimrc" && hf=$HOME/_vimrc
+    fi
 
     tf=`readlink -f $file`
-    [ "`readlink -f $hf`" = "$tf" ] && continue
+    [ "`readlink -f $hf`" = "$tf" ] && echo "$file" already linked && return
 
     if [ -e $hf ]
     then
         echo $hf exists 1>&2
     elif $win
-        echo "link $tf -> $hf"
-        [ -d $hf ] && mklink \d $tf $hf || mklink $tf $hf
+    then
+        tf=`cygpath -w $tf`
+        hf=`cygpath -w $hf`
+        echo "mklink $tf -> $hf"
+        [ -d $hf ] && mklink \d $hf $tf || mklink $hf $tf
     else
         echo "link $tf -> $hf"
         ln -s $tf $hf
@@ -28,7 +38,14 @@ function dolink()
 }
     
 
-for lfile in * */bash_aliases */*rc
+for lfile in * */bash_aliases
 do
-    dolink $lfile
+    if [ $lfile != "init.sh" ]
+    then
+        dolink $lfile
+    fi
+    if [ -e $lfile/"$lfile"rc ]
+    then
+        dolink $lfile/"$lfile"rc
+    fi
 done
