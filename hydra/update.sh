5d069c1fb7d82ea89053852a1e713bfb7184005c
Ryan Wooden <rygwdn@gmail.com>
2014-07-08 10:08:53 -0300

    add Hydra.app config.

---
+++ hydra/update.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+curl 'https://raw.githubusercontent.com/sdegutis/dotfiles/osx/home/.hydra/grid.lua' -o ./grid.lua
+curl 'https://raw.githubusercontent.com/sdegutis/dotfiles/osx/home/.hydra/init' -o ./sdegutis.lua
