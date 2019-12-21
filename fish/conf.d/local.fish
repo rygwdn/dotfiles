164e6f007bb46f6aa5f832242aee81f85b598b3c
Ryan Wooden <rygwdn@gmail.com>
2019-12-21 14:28:41 -0400

    ignore local.fish.

--- fish/conf.d/local.fish
+++
@@ -1,8 +0,0 @@
-set win "/mnt/c/Users/ryan.wooden"
-abbr win cd $win
-
-set is '/mnt/c/Users/ryan.wooden/intellitrack-service'
-abbr is cd $is
-
-set isw '/mnt/c/Users/ryan.wooden/intellitrack-service-wip'
-abbr isw cd $isw
