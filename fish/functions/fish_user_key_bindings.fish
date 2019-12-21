d90634adfff6e3f2fa969aef0f31d8977e7fcc30
Ryan Wooden <rygwdn@gmail.com>
2019-12-21 14:30:13 -0400

    update fish plugins.

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,7 +1,7 @@
 # Defined in /tmp/fish.WlCnUw/fish_user_key_bindings.fish @ line 2
 function fish_user_key_bindings
 	fish_vi_key_bindings
-	fzf_key_bindings
+        type -q fzf_key_bindings; and fzf_key_bindings
 
 	# ctrl-o to push current line to next prompt
 	bind \co push-line
