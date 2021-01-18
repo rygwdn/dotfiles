d9f273d5b8f5ec4142ed8f9d7eef784a52fb5c31
Ryan Wooden <rygwdn@gmail.com>
2021-01-18 15:43:28 -0400

    more windows fixes.

--- init.py
+++ init.py
@@ -2,6 +2,7 @@
 
 import argparse
 import platform
+import os
 from pathlib import Path
 
 all_platforms = [
@@ -19,6 +20,8 @@ all_platforms = [
 windows_links = all_platforms + [
     ("tridactylrc", "_tridactylrc"),
     ("vim", "vimfiles"),
+    #("vim", "AppData/Local/nvim"),
+    ("vim", ".config/nvim"),
     ("vim/_vimrc",  "_vimrc"),
     ("windows/profile.ps1",  "Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1"),
 ]
@@ -34,7 +37,8 @@ unix_links = all_platforms + [
 
 def dolink(file, destname=None):
     src = Path(file).resolve()
-    dest = Path(Path.home(), destname or "." + src.name).resolve()
+    orig_dest = Path(Path.home(), destname or "." + src.name)
+    dest = orig_dest.resolve()
 
     destname = "~/" + (destname or str(dest.relative_to(Path.home())))
     srcname = src.relative_to(Path.cwd())
@@ -43,7 +47,9 @@ def dolink(file, destname=None):
         raise Exception(f"{src} does not exist!")
 
     if src.exists() and dest.exists() and src.samefile(dest):
-        print(f"✔ {srcname}")
+        #destname = orig_dest
+        #srcname = src
+        print(f"✔ {srcname} -> {destname}")
     elif dest.exists() and dest.is_file():
         with dest.open() as dFile:
             with src.open() as sFile:
@@ -59,6 +65,22 @@ def dolink(file, destname=None):
         print(f"↷ {srcname} -> {destname}")
         return lambda: dest.symlink_to(src.resolve())
 
+def check_windows_env():
+    env_issues = []
+
+    home = Path.home()
+    expected_config = f"{home}\\.config"
+    if 'XDG_CONFIG_HOME' not in os.environ or os.environ['XDG_CONFIG_HOME'] != expected_config:
+        env_issues.append(f'Please set XDG_CONFIG_HOME to "{expected_config}"')
+    if 'HOME' not in os.environ or os.environ["HOME"] != home:
+        env_issues.append(f'Please set HOME to "{home}"')
+
+    if env_issues:
+        print("-------------------------------------")
+        for ei in env_issues:
+            print(ei)
+        print("-------------------------------------")
+
 def main():
     parser = argparse.ArgumentParser(description='Initialize dotfiles')
     parser.add_argument('--dry', action='store_true', help='do a dry run')
@@ -68,6 +90,8 @@ def main():
 
     is_windows = platform.system() == "Windows"
     links = is_windows and windows_links or unix_links
+    if is_windows:
+        check_windows_env()
 
     operations = []
     for link in links:
