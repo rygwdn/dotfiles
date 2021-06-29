10e55205ccf03aefc8aca70eea17d34ad83352c2
Ryan Wooden <rygwdn@gmail.com>
2021-06-29 09:15:18 -0300

    windows updates.

--- windows/profile.ps1
+++ windows/profile.ps1
@@ -1,8 +1,8 @@
 Set-PSReadlineKeyHandler -Key Tab -Function Complete
 Set-PSReadlineKeyHandler -Key ctrl+d -Function ViExit
 Set-PSReadlineOption -BellStyle Visual 
-Set-Alias -Name yt -Value youtube-dl.exe
 
+Set-Alias -Name yt -Value youtube-dl.exe
 
 if ($env:WT_SESSION) {
   # Only use powerline theme in windows terminal, not standard terminal
