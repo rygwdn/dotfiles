2d7b0e6c2c837fd6eb8120095ad9e0355f4cfe3d
Ryan Wooden <rygwdn@gmail.com>
2024-02-13 15:23:29 -0400

    use pbcopy.

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -4,22 +4,8 @@
 vim.opt.title = true
 vim.opt.diffopt = { "filler", "iwhite" } -- ignore all whitespace and sync
 
--- always use OSC 52 with tmux
-if os.getenv("TMUX") then
-  -- local clipboard_module = (vim.fn.has("nvim-0.10") and "vim.ui.clipboard.osc52" or "osc52")
-  local clipboard_module = "osc52"
-  vim.g.clipboard = {
-    name = "Local OSC52",
-    copy = {
-      ["+"] = require(clipboard_module).copy("+"),
-      ["*"] = require(clipboard_module).copy("*"),
-    },
-    paste = {
-      ["+"] = require(clipboard_module).paste("+"),
-      ["*"] = require(clipboard_module).paste("*"),
-    },
-  }
-elseif vim.fn.executable("pbcopy") == 1 then
+-- use pbcopy on linux servers with fake pbcopy executable
+if vim.fn.executable("pbcopy") == 1 then
   vim.g.clipboard = {
     name = "pbcopy",
     copy = {
