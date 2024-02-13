05a2db5215dcd0514730e69eb8bfb378a5208d55
Ryan Wooden <rygwdn@gmail.com>
2024-02-13 12:17:39 -0400

    add clipboard config

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -3,3 +3,32 @@
 
 vim.opt.title = true
 vim.opt.diffopt = { "filler", "iwhite" } -- ignore all whitespace and sync
+
+-- always use OSC 52 with tmux
+if os.getenv("TMUX") then
+  -- local clipboard_module = (vim.fn.has("nvim-0.10") and "vim.ui.clipboard.osc52" or "osc52")
+  local clipboard_module = "osc52"
+  vim.g.clipboard = {
+    name = "Local OSC52",
+    copy = {
+      ["+"] = require(clipboard_module).copy("+"),
+      ["*"] = require(clipboard_module).copy("*"),
+    },
+    paste = {
+      ["+"] = require(clipboard_module).paste("+"),
+      ["*"] = require(clipboard_module).paste("*"),
+    },
+  }
+elseif vim.fn.executable("pbcopy") == 1 then
+  vim.g.clipboard = {
+    name = "pbcopy",
+    copy = {
+      ["+"] = { "pbcopy" },
+      ["*"] = { "pbcopy" },
+    },
+    paste = {
+      ["+"] = { "pbpaste" },
+      ["*"] = { "pbpaste" },
+    },
+  }
+end
