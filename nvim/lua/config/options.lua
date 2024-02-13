b5af19b6b7c7219b0c6f320d6f8a431f605d57b1
Ryan Wooden <ryan.wooden@shopify.com>
2024-02-13 19:36:52 +0000

    use osc and pbpaste.

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -3,18 +3,3 @@
 
 vim.opt.title = true
 vim.opt.diffopt = { "filler", "iwhite" } -- ignore all whitespace and sync
-
--- use pbcopy on linux servers with fake pbcopy executable
-if vim.fn.executable("pbcopy") == 1 then
-  vim.g.clipboard = {
-    name = "pbcopy",
-    copy = {
-      ["+"] = { "pbcopy" },
-      ["*"] = { "pbcopy" },
-    },
-    paste = {
-      ["+"] = { "pbpaste" },
-      ["*"] = { "pbpaste" },
-    },
-  }
-end
--- nvim/lua/plugins/specs.lua
+++ nvim/lua/plugins/specs.lua
@@ -5,6 +5,30 @@ return {
   { "rygwdn/vim-tmux-navigator" },
 
   {
+    "ojroques/nvim-osc52",
+    config = function()
+      local osc = require("osc52")
+
+      local function copy(lines, _)
+        osc.copy(table.concat(lines, "\n"))
+      end
+
+      local paste = vim.fn.executable("pbpaste") == 1 and { "pbpaste" }
+        or function()
+          return { vim.fn.split(vim.fn.getreg(""), "\n"), vim.fn.getregtype("") }
+        end
+
+      if vim.fn.executable("pbpaste") == 1 then
+        vim.g.clipboard = {
+          name = "osc52 & pbpaste",
+          copy = { ["+"] = copy, ["*"] = copy },
+          paste = { ["+"] = paste, ["*"] = paste },
+        }
+      end
+    end,
+  },
+
+  {
     "stevearc/oil.nvim",
     opts = {},
     dependencies = { "nvim-tree/nvim-web-devicons" },
