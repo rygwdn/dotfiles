0dec3bc7d4c96ea7835c74b7556e7f3ececd7392
Ryan Wooden <rygwdn@gmail.com>
2024-02-21 12:05:29 -0400

    fix tmux navigator.

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -2,7 +2,24 @@ return {
   { "tpope/vim-eunuch", vscode = true },
   { "tpope/vim-git" },
   { "tpope/vim-fugitive" },
-  { "rygwdn/vim-tmux-navigator" },
+
+  {
+    "rygwdn/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+    },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
+  },
 
   {
     "folke/flash.nvim",
