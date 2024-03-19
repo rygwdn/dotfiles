8c224e46ce0b50f4b416e917aad1a65bf14e9e23
Ryan Wooden <rygwdn@gmail.com>
2024-03-19 16:49:02 -0300

    add hotkey.

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -5,6 +5,8 @@ local u = require("utils")
 -- for more apps, run
 -- $ ls /Applications
 hs.hotkey.bind({}, "F1", u.toggleApp("Kitty"))
+hs.hotkey.bind({ "shift" }, "F1", u.toggleApp("Obsidian"))
+
 --hs.hotkey.bind(cmd2, "1", u.weakFocus("Slack"))
 --hs.hotkey.bind(cmd2, "2", u.focusApp("Google Chrome"))
 --hs.hotkey.bind(cmd2, "3", u.weakFocus("IntelliJ IDEA"))
