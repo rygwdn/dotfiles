685fd3dcec5c6a53674883380b95c883dc7b7519
Ryan Wooden <ryan.wooden@shopify.com>
2026-01-05 14:21:58 -0400

    fix graphite commands.

--- fish/config.fish
+++ fish/config.fish
@@ -31,8 +31,8 @@ end
 
 _abbr_if_gt co 'gt checkout' 'git checkout'
 _abbr_if_gt ci 'gt modify --commit' 'git commit'
-_abbr_if_gt pff 'gt sync --no-restack && git pull --ff-only' 'git pull --ff-only'
-_abbr_if_gt gfu 'gt sync --no-restack' 'git fetch --prune --tags origin'
+_abbr_if_gt pff 'gt sync --no-restack  --no-interactive && git merge --ff-only' 'git pull --ff-only'
+_abbr_if_gt gfu 'gt sync --no-restack --no-interactive' 'git fetch --prune --tags origin'
 _abbr_if_gt pof '' 'git push origin --force-with-lease'
 _abbr_if_gt gph '' 'git push origin --set-upstream HEAD'
 _abbr_if_gt rbc '' 'git rebase --continue'
