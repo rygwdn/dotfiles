a33f9985b840cb6f2ee5ff9af10764a5ce1ababb
Ryan Wooden <rygwdn@gmail.com>
2026-01-05 20:47:53 -0400

    nvim cleanup

--- nvim/lua/plugins/ruby.lua
+++ nvim/lua/plugins/ruby.lua
@@ -1,10 +1,10 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, {
-        "ruby",
-      })
-    end,
-  },
+  -- {
+  --   "nvim-treesitter/nvim-treesitter",
+  --   opts = function(_, opts)
+  --     vim.list_extend(opts.ensure_installed, {
+  --       "ruby",
+  --     })
+  --   end,
+  -- },
 }
